3cd01f2bed8570f29139eea200d00f6e59eb0338
SaulMoralesS <79545038+SaulMoralesS@users.noreply.github.com>
2021-03-03 19:58:38 -0600

    Add files via upload

---
+++ Introduccion.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+float a,b,multi;
+int main(void)
+
+    {
+        cout<< "\n Ingrese el 1er numero a multi\n";
+        cin>> a;
+        cout<<"\n Ingrese el 2do numero a multi\n";
+        cin>> b;
+        multi=a*b;
+        cout<<"\n El resultado de la multi es\n";
+        cout<< multi;
+        cout<< endl;
+        return 0;
+}
